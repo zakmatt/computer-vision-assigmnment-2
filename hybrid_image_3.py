6ebc287e2c158cc5e1aa3884ddbe8a3143ed25ff
matthew-zak <matthew.zak@tooploox.com>
2017-02-07 12:43:24 -0500

    use default filtering functions

--- hybrid_image_3.py
+++ hybrid_image_3.py
@@ -29,7 +29,7 @@ def sobel_test(image_low, image_high, size, sigma):
     oryginal_high = image_high.astype('float32') - cv2.GaussianBlur(image_high, (size, size), 17).astype('float32')
     oryginal_high = controllers.rescale(oryginal_high)
     oryginal = controllers.rescale(image_low + oryginal_high).astype('float32')
-    comparison = np.sqrt((oryginal - sobel_output) ** 2)
+    comparison = np.sqrt((oryginal_high - sobel_high) ** 2)
     comparison = controllers.rescale(comparison)
     cv2.imwrite('results/sobel/sobel_comparison_%d.jpeg' % sigma, comparison.astype('uint8'))
     
@@ -42,12 +42,14 @@ def dog_test(image_low, image_high, size, sigma_1, sigma_2):
     oryginal_output = controllers.rescale(image_low + oryginal_high).astype('float32')
     
     d_o_g_filter = controllers.d_o_g(size, sigma_1, sigma_2)
-    image_high = controllers.filtering(image_high, d_o_g_filter)
+    image_high[:, :, 0] = np.array(cv2.filter2D(image_high[:, :, 0], -1, d_o_g_filter), dtype = np.float32)
+    image_high[:, :, 1] = np.array(cv2.filter2D(image_high[:, :, 1], -1, d_o_g_filter), dtype = np.float32)
+    image_high[:, :, 2] = np.array(cv2.filter2D(image_high[:, :, 2], -1, d_o_g_filter), dtype = np.float32)
     image_high = controllers.rescale(image_high)
     dog_output = controllers.rescale(image_low + image_high).astype('float32')
     cv2.imwrite('results/dog/dog_%d_%d_%d.jpeg' % (size, sigma_1, sigma_2), dog_output.astype('uint8'))
     
-    comparison = np.sqrt((oryginal_output - dog_output) ** 2)
+    comparison = np.sqrt((oryginal_high - image_high) ** 2)
     comparison = controllers.rescale(comparison)
     cv2.imwrite('results/dog/dog_comparison_%d_%d_%d.jpeg' % (size, sigma_1, sigma_2), comparison.astype('uint8'))
     
@@ -61,12 +63,14 @@ def laplacian_test(image_low, image_high, size, sigma):
     oryginal_output = controllers.rescale(image_low + oryginal_high).astype('float32')
     
     kernel_l_o_g = controllers.l_o_g_kernel(size, sigma)
-    image_high = controllers.filtering(image_high, kernel_l_o_g)
+    image_high[:, :, 0] = np.array(cv2.filter2D(image_high[:, :, 0], -1, kernel_l_o_g), dtype = np.float32)
+    image_high[:, :, 1] = np.array(cv2.filter2D(image_high[:, :, 1], -1, kernel_l_o_g), dtype = np.float32)
+    image_high[:, :, 2] = np.array(cv2.filter2D(image_high[:, :, 2], -1, kernel_l_o_g), dtype = np.float32)
     image_high = controllers.rescale(image_high)
     log_output = controllers.rescale(image_low + image_high).astype('float32')
     cv2.imwrite('results/laplacian/log_%d_%d.jpeg' % (size, sigma), log_output.astype('uint8'))
     
-    comparison = np.sqrt((oryginal_output - log_output) ** 2)
+    comparison = np.sqrt((oryginal_high - image_high) ** 2)
     comparison = controllers.rescale(comparison)
     cv2.imwrite('results/laplacian/log_comparison_%d_%d.jpeg' % (size, sigma), comparison.astype('uint8'))
     
@@ -90,3 +94,54 @@ if __name__ == '__main__':
     laplacian_test(image_low, image_high, 21, 10)
     laplacian_test(image_low, image_high, 31, 25)
     laplacian_test(image_low, image_high, 41, 40)
+    '''
+    kernel_1 = controllers.GaussianKernel(33, 60)
+    image_low = controllers.open_image('data/dog.bmp', False).astype('float32')
+    image_high = controllers.open_image('data/cat.bmp', False).astype('float32')
+    
+    image_low = controllers.filtering(image_low, kernel_1)
+    image_low = controllers.rescale(image_low)
+    
+    image_high = controllers.sobel_filtering(image_high)
+    image_high = controllers.rescale(image_high)
+    output = image_low + image_high
+    output = controllers.rescale(image_low + image_high)
+    plt.imshow(output.astype('uint8'))
+    plt.show()
+    cv2.imwrite('cement.jpeg', output.astype('uint8'))
+    '''
+    # DoG - WORKING!
+    '''
+    kernel_1 = controllers.GaussianKernel(21, 11)
+    image_low = controllers.open_image('data/dog.bmp', False).astype('float32')
+    image_high = controllers.open_image('data/cat.bmp', False).astype('float32')
+    
+    image_low = controllers.filtering(image_low, kernel_1)
+    image_low = controllers.rescale(image_low)
+    d_o_g = controllers.d_o_g(11, 30, 10)
+    temp = d_o_g#(d_o_g - np.min(d_o_g))/(np.max(d_o_g) - np.min(d_o_g))
+    image_high = controllers.filtering(image_high, temp)
+    image_high = controllers.rescale(image_high)
+    output = controllers.rescale(image_low + image_high)
+    plt.imshow(output.astype('uint8'))
+    plt.show()
+    cv2.imwrite('output.jpeg', output)
+    '''
+    # Laplacian of Gaussians
+    '''
+    kernel_1 = controllers.GaussianKernel(33, 20)
+    image_low = controllers.open_image('data/dog.bmp', False).astype('float32')
+    image_high = controllers.open_image('data/cat.bmp', False).astype('float32')
+    
+    image_low = controllers.filtering(image_low, kernel_1)
+    image_low = controllers.rescale(image_low)
+    #image_high = controllers.l_o_g_filtering(image_high)
+    kernel_l_o_g = controllers.l_o_g_kernel(17, 11)
+    image_high = controllers.filtering(image_high, kernel_l_o_g)
+    image_high = controllers.rescale(image_high)
+    output = image_low + image_high
+    output = controllers.rescale(output)
+    plt.imshow(output.astype('uint8'))
+    plt.show()
+    cv2.imwrite('log.jpeg', output)
+    '''
